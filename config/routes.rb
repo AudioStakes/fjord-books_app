666339a1ae4dc999638ea5642254cf36e093bdeb
AudioStakes <daisuke.sato@enishi-tech.com>
2021-07-30 13:52:50 +0900

    アプリの利用にはログインを必須とした

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:index, :show]
   resources :books
