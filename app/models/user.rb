855024538e9d9cbdd94fbce088a192a14d492714
AudioStakes <daisuke.sato@enishi-tech.com>
2021-08-04 10:44:47 +0900

    アップロード可能なファイルはjpg, png, gifの3種類だけに限定するようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :icon
+
+  validate :content_type_of_icon_must_be_allowed
+
+  private
+
+  def content_type_of_icon_must_be_allowed
+    allowed_file_types = ['image/png', 'image/jpeg', 'image/gif']
+
+    return if icon.content_type.in?(allowed_file_types)
+
+    errors.add(:icon, :invalid)
+  end
 end
