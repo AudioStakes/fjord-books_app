df5ae04385d9aaa0d2e237ffcf0a1cd6e0aec0c1
AudioStakes <daisuke.sato@enishi-tech.com>
2021-08-04 17:10:09 +0900

    ユーザー同士がフォローし合えるようにモデルと関連を追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :active_follows, class_name: 'Follow', foreign_key: :follower_id, dependent: :destroy, inverse_of: :follower
+  has_many :followings, through: :active_follows
+
+  has_many :passive_follows, class_name: 'Follow', foreign_key: :following_id, dependent: :destroy, inverse_of: :following
+  has_many :followers, through: :passive_follows
 end
