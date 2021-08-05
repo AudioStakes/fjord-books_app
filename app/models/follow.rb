dae5a2ed5089f6f06fcdc5a1d055f9c19e47fda1
AudioStakes <daisuke.sato@enishi-tech.com>
2021-08-05 17:12:08 +0900

    自分自身をフォローできないようにした

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -5,4 +5,13 @@ class Follow < ApplicationRecord
   belongs_to :following, class_name: 'User'
 
   validates :follower, uniqueness: { scope: :following }
+  validate :must_not_follow_youlself
+
+  private
+
+  def must_not_follow_youlself
+    return unless follower == following
+
+    errors.add(:base, :must_not_follow_youlself)
+  end
 end
