639bf90459a3cc55035151d4e879bc380bf1705d
AudioStakes <daisuke.sato@enishi-tech.com>
2021-08-16 11:23:42 +0900

    タイポを修正

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -5,13 +5,13 @@ class Follow < ApplicationRecord
   belongs_to :following, class_name: 'User'
 
   validates :follower, uniqueness: { scope: :following }
-  validate :must_not_follow_youlself
+  validate :must_not_follow_yourself
 
   private
 
-  def must_not_follow_youlself
+  def must_not_follow_yourself
     return unless follower == following
 
-    errors.add(:base, :must_not_follow_youlself)
+    errors.add(:base, :must_not_follow_yourself)
   end
 end
