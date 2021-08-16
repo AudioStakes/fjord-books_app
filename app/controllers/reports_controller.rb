275041c39802ea1f3ce308212d502fac573dc590
AudioStakes <daisuke.sato@enishi-tech.com>
2021-08-16 17:38:42 +0900

    日報の CRUD 機能の日本語表示に対応

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -21,7 +21,7 @@ class ReportsController < ApplicationController
     @report = current_user.reports.new(report_params)
 
     if @report.save
-      redirect_to @report, notice: 'Report was successfully created.'
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
     end
@@ -31,7 +31,7 @@ class ReportsController < ApplicationController
     @report = current_user.reports.find(params[:id])
 
     if @report.update(report_params)
-      redirect_to @report, notice: 'Report was successfully updated.'
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -42,7 +42,7 @@ class ReportsController < ApplicationController
 
     @report.destroy
 
-    redirect_to reports_url, notice: 'Report was successfully destroyed.'
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
