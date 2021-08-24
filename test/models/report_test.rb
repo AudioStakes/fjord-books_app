b03165c192ec90c88a64ae15897036d84767a721
AudioStakes <daisuke.sato@enishi-tech.com>
2021-08-24 17:13:10 +0900

    Report モデルのインスタンスメソッドのテストを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,24 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?(target_user) returns true when created by the target user' do
+    alice  = create(:user)
+    report = create(:report, user: alice)
+
+    assert report.editable?(alice)
+  end
+
+  test '#editable?(target_user) returns false when not created by the target user' do
+    alice  = create(:user)
+    bob    = create(:user)
+    report = create(:report, user: bob)
+
+    assert_not report.editable?(alice)
+  end
+
+  test '#created_on returns a created date' do
+    report = create(:report)
+
+    assert_equal report.created_at.to_date, report.created_on
+  end
 end
