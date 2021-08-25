97209a524ae56a52d2a3d1ecfd16b7082b27a952
AudioStakes <daisuke.sato@enishi-tech.com>
2021-08-25 16:10:39 +0900

    本の CRUD のテストが動くように修正した

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,44 +4,78 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
+    user = create(:user)
+
+    visit root_url
+
+    fill_in 'Eメール', with: user.email
+    fill_in 'パスワード', with: user.password
+
+    click_on 'ログイン'
   end
 
   test 'visiting the index' do
+    book = create(:book)
+
     visit books_url
-    assert_selector 'h1', text: 'Books'
+
+    assert_selector 'h1', text: '本'
+    assert_text book.title
+    assert_text book.memo
+    assert_text book.author
   end
 
   test 'creating a Book' do
+    book = build(:book)
+
     visit books_url
-    click_on 'New Book'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    click_on '新規作成'
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    fill_in 'タイトル', with: book.title
+    fill_in 'メモ', with: book.memo
+    fill_in '著者', with: book.author
+
+    click_on '登録する'
+
+    assert_text '本が作成されました。'
+    assert_text book.title
+    assert_text book.memo
+    assert_text book.author
   end
 
   test 'updating a Book' do
+    create(:book)
+    book = build(:book)
+
     visit books_url
-    click_on 'Edit', match: :first
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    click_on '編集', exact: true
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    fill_in 'タイトル', with: book.title
+    fill_in 'メモ', with: book.memo
+    fill_in '著者', with: book.author
+
+    click_on '更新する'
+
+    assert_text '本が更新されました。'
+    assert_text book.title
+    assert_text book.memo
+    assert_text book.author
   end
 
   test 'destroying a Book' do
+    book = create(:book)
+
     visit books_url
+
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on '削除'
     end
 
-    assert_text 'Book was successfully destroyed'
+    assert_text '本が削除されました。'
+    assert_no_text book.title
+    assert_no_text book.memo
+    assert_no_text book.author
   end
 end
